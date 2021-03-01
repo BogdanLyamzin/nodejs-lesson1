ab5f6643c21cfd782e45eb113c4b28b33549f5be
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-03-01 19:18:26 +0200

    add new lesson

---
+++ 3. node-module-es6-import/users.js
@@ -0,0 +1,9 @@
+const managers = ["Иван", "Олег", "Мефистофель"];
+const admins = ["Александр", "Фауст", "Мона Лиза"];
+
+exports.admins = admins;
+exports.managers = managers;
+
+// module.exports = {
+//     new: "mem"
+// }
